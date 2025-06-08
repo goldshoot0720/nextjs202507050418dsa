83e72d052c4ff761170ba384e443d850d091d14f
goldshoot0720 <goldshoot0720@gmail.com>
2025-06-08 15:25:13 +0800

    nextjs202507050418dsa init

---
+++ nextjs202507050418dsa-git.bat
@@ -0,0 +1,13 @@
+git init
+
+git add .
+ 
+git commit -m "nextjs202507050418dsa init"
+
+git remote add origin https://github.com/goldshoot0720/nextjs202507050418dsa.git
+
+git checkout -b master
+
+git push -u origin master --force
+
+pause
---
+++ nextjs202507050418dsa-open.bat
@@ -0,0 +1,4 @@
+cd "C:\Users\chbon\Documents\20250705\nextjs202507050418dsa"
+explorer http://localhost:3000
+npm run dev
+pause
