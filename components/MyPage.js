ca342b453fc1857f2fa2274ba0cac33471903fdf
goldshoot0720 <goldshoot0720@gmail.com>
2025-06-09 02:32:13 +0800

    Stack

--- components/MyPage.js
+++ components/MyPage.js
@@ -1,9 +1,4 @@
-"use client";
-import React, { useState } from "react";
-
 export default function MyPage(props) {
-  const [isHovered, setIsHovered] = useState(false);
-
   return (
     <div>
       <h1>{props.title}</h1>
